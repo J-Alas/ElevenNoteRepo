162289c27198e2c70703cf3fd4a39133938b2608
Jon Alas <jalas121@gmail.com>
2021-11-30 17:26:56 -0600

    fifth

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Services
 {
-    class NoteService
+    public class NoteService
     {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate Model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = Model.Title,
+                    Content = Model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+
+            }
+        }
+
     }
 }
---
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,44 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebApi.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+    }
+}
