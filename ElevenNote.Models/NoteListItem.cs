9828959ab5b85734d931e1566d5da44f9ece89f0
Jon Alas <jalas121@gmail.com>
2021-11-30 17:10:02 -0600

    fourth

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
